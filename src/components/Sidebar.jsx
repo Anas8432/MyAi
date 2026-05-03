d5ffd248c2492b26016c10e198fd7c23717ada94
Anas9921 <mkanaskhan9921@gmail.com>
2026-05-03 23:26:19 +0530

    Gemini clone with new features in React js

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,16 +16,16 @@ const Sidebar = () => {
   }
 
   return (
-    <div className="min-h-screen inline-flex flex-col justify-between bg-[#e4e7eb] py-[25px] px-[15px]">
+    <div className="min-h-screen inline-flex flex-col justify-between bg-[#000000] py-[25px] px-[15px]">
       <div>
         <IoMenu
           onClick={() => setExtended(!extended)}
-          className="text-2xl block cursor-pointer"
+          className="text-2xl block cursor-pointer text-gray-50"
         />
 
         <div
           onClick={() => newChat()}
-          className="mt-[10px] inline-flex items-center gap-[10px] py-[10px] px-[15px] text-[14px] text-gray-500 cursor-pointer bg-gray-300 rounded-full"
+          className="mt-[10px] inline-flex items-center gap-[10px] py-[10px] px-[15px] text-[14px] text-gray-500 cursor-pointer bg-gray-50 rounded-full"
         >
           <FaPlus className="text-2xl" />
 
@@ -52,17 +52,17 @@ const Sidebar = () => {
       </div>
 
       <div className="flex flex-col">
-        <div className="flex items-center gap-2 p-2 pr-10 rounded-[50px] text-slate-700 cursor-pointer hover:bg-gray-300">
+        <div className="flex items-center gap-2 p-2 pr-10 rounded-[50px] text-slate-50 cursor-pointer hover:bg-gray-600">
           <FaQuestion className="text-2xl" />
           {extended && <p>Help</p>}
         </div>
 
-        <div className="flex items-center gap-2 p-2 pr-10 rounded-[50px] text-slate-700 cursor-pointer hover:bg-gray-300">
+        <div className="flex items-center gap-2 p-2 pr-10 rounded-[50px] text-slate-50 cursor-pointer hover:bg-gray-600">
           <MdHistory className="text-2xl" />
           {extended && <p>Activity</p>}
         </div>
 
-        <div className="flex items-center gap-2 p-2 pr-10 rounded-[50px] text-slate-700 cursor-pointer hover:bg-gray-300">
+        <div className="flex items-center gap-2 p-2 pr-10 rounded-[50px] text-slate-50 cursor-pointer hover:bg-gray-600">
           <IoSettings className="text-2xl" />
           {extended && <p>Settings</p>}
         </div>
